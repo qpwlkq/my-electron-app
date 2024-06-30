31cf15630eadd79a7787c4c67a656072ad7559ac
winston.qin <winston.qin@temu.com>
2024-06-30 23:28:53 +0800

    add preload.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const {app, BrowserWindow} = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width:800,
         height:600,
-        autoHideMenuBar:true
+        webPreferences: {
+            preload: path.join(__dirname, 'src/preload.js')
+        }
     })
-    win.loadFile('./src/index.html')
+    win.loadFile('src/index.html')
 }
 
 app.on('ready', () => {
