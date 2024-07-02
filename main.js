d6594e4a27ac7f153a76c2dc661a07baf5240771
winston.qin <winston.qin@temu.com>
2024-07-02 22:22:56 +0800

    IPC practice

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('node:path')
+const { app, BrowserWindow, Menu, ipcMain } = require("electron/main");
+const path = require("node:path");
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width:800,
-        height:600,
-        webPreferences: {
-            preload: path.join(__dirname, 'src/preload.js')
-        }
-    })
-    win.loadFile('src/index.html')
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, "./src/preload.js"),
+    },
+  });
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: app.name,
+      submenu: [
+        {
+          click: () => mainWindow.webContents.send("update-counter", 1),
+          label: "Increment",
+        },
+        {
+          click: () => mainWindow.webContents.send("update-counter", -1),
+          label: "Decrement",
+        },
+      ],
+    },
+  ]);
+
+  Menu.setApplicationMenu(menu);
+  mainWindow.loadFile("./src/index.html");
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 }
 
-app.on('ready', () => {
-    createWindow()
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
-})
+app.whenReady().then(() => {
+  ipcMain.on("counter-value", (_event, value) => {
+    console.log(value); // will print value to Node console
+  });
+  createWindow();
 
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
--- src/preload.js
+++ src/preload.js
@@ -1,11 +1,7 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) {
-            element.innerText = text
-        }
-    }
-    for (const dependency of ['chrome', 'node', 'electron']) {
-        replaceText(`${dependency}-version`, process.versions[dependency])
-    }
-})
+const { contextBridge, ipcRenderer } = require("electron/renderer");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  onUpdateCounter: (callback) =>
+    ipcRenderer.on("update-counter", (_event, value) => callback(value)),
+  counterValue: (value) => ipcRenderer.send("counter-value", value),
+});
